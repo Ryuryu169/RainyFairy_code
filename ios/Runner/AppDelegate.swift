26e53662dda0f07bbf79be26bb1be129a5dc1301
Ryuryu169 <ryusin916@gmail.com>
2022-11-22 20:41:16 +0900

    11-22 Commit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,21 @@
 import UIKit
 import Flutter
+import FirebaseCore
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+      [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
